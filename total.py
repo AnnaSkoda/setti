8e84c0607cdef50050a225bd9fb03bf2ce269a8c
AnnaSkoda <zbaraz77@gmail.com>
2019-07-16 14:42:51 +0300

    test_2

--- total.py
+++ total.py
@@ -2,7 +2,7 @@ def total(initial=5, *numbers, **keywords):
     count = initial
     for number in numbers:
         count += number
-        for key in keywords:
+    for key in keywords:
             count += keywords[key]
-        return count
+    return count
 print(total(10, 1, 2, 3, vegetables=50, fruits=100))
